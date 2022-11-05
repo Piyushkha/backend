83cc40d54c22692e21684ee17032f17a6b66f7a1
Piyush khatri <98674476+Piyushkha@users.noreply.github.com>
2022-11-05 20:44:55 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose')
-const uri = "mongodb://44.207.233.97/mytask"
+const dbUser ="piyushkhatridb"
+const dbPassword="piyush123"
+const hostName="cluster0.adzul6t.mongodb.net"
+const dbName="blogdb"
+const localDB = `mongodb+srv://${dbUser}:${dbPassword}@${hostName}/${dbName}?retryWrites=true&w=majority`;
+// mongoose.connect(process.env.MONGODB_URI || localDB,
+mongoose.connect(localDB,
 
-mongoose.connect(uri,(err,cb)=>{
-    if(err) throw err
-    console.log("Database connected");
-})
+
+    { useNewUrlParser: true, useUnifiedTopology: true }
+    ).then(() => {
+    console.log(`DB connection successfull`);
+}).catch((error) => {
+    console.log(error);
+});
+mongoose.Promise = global.Promise;
+
+
